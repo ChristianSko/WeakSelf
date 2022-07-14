76d07e499b5ff4b6bba185ae07de6473fb869189
Christian Skorobogatow <christiansko@icloud.com>
2022-07-14 16:38:13 +0200

    Initial Commit

---
+++ WeakSelf/WeakSelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeakSelfApp.swift
+//  WeakSelf
+//
+//  Created by Christian Skorobogatow on 14/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WeakSelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
